5fe8f9e3b387c7ce09460072ea98ee4cbcc6a96a
KenDarmawan <kenzodarmawan968@gmail.com>
2025-12-14 01:55:58 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -15,6 +15,17 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase
 // 5. Click on the web app icon (</>)
 // 6. Copy the configuration object
 
+// isi dengan firebase config pribadi
+
+//const firebaseConfig = {
+//  apiKey: "YOUR_API_KEY",
+//  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+//  projectId: "YOUR_PROJECT_ID",
+//  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+//  messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+//  appId: "YOUR_APP_ID"
+//};
+
 const firebaseConfig = {
     apiKey: "AIzaSyAdEFtL16_llO9ymMMbh-ZAGVsvdgVk_cc",
     authDomain: "personal-finance-tracker-f603e.firebaseapp.com",
